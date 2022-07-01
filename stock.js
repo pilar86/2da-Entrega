32749247123c7281dc6ddac3bd6680e63464149c
Pilar Gomez <pilarg@gmail.com>
2022-06-30 22:34:46 -0300

    segunda entrega

---
+++ stock.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    {id: 1, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-camaro.jpg'},
+    {id: 2, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-but-1.jpg'},
+    {id: 3, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-but-2.jpg'},
+    {id: 4, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-but-3.jpg'},
+    {id: 5, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-FLamp1.jpg'},
+    {id: 6, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-HLamp1.jpg'},
+    {id: 7, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-HLamp2.jpg'},
+    {id: 8, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-MArrime1.jpg'},
+    {id: 9, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-MCentro1.jpg'},
+    {id: 10, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-silla1.jpg'},
+    {id: 11, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-tab1.jpg'},
+    {id: 12, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-TLamp1.jpg'},
+]

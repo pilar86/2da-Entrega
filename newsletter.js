81dee7ad4e9321b50c2c4ad5f5cbffd7e633f3ba
Pilar Gomez <pilarg@gmail.com>
2022-07-06 23:00:58 -0300

    sweet alert

---
+++ newsletter.js
@@ -0,0 +1,20 @@
+const formulario = document.getElementById('form')
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    let mail = document.getElementById('email-form').value
+
+    
+})
+
+
+
+Swal.fire({
+    title: 'Suscribite al Newsletter!',
+    text: 'No te pierdas de las novedades y descuentos.',
+    imageUrl: './img/backNews.jpg',
+    imageWidth: 400,
+    imageHeight: 260,
+    
+    imageAlt: 'Custom image',
+  })
--- stock.js
+++ stock.js
@@ -1,8 +1,8 @@
 let stockProductos = [
-    {id: 1, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-camaro.jpg'},
-    {id: 2, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-but-1.jpg'},
-    {id: 3, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-but-2.jpg'},
-    {id: 4, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-but-3.jpg'},
+    {id: 1, nombre: "SOFA CAMARO", cantidad: 1, precio: 39000, img: './img/BoC-camaro.jpg'},
+    {id: 2, nombre: "BUTACA i", cantidad: 1, precio: 146000, img: './img/BoC-but-1.jpg'},
+    {id: 3, nombre: "BUTACA ii", cantidad: 1, precio: 180000, img: './img/BoC-but-2.jpg'},
+    {id: 4, nombre: "BUTACA Imola", cantidad: 1, precio: 23500, img: './img/BoC-but-3.jpg'},
     {id: 5, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-FLamp1.jpg'},
     {id: 6, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-HLamp1.jpg'},
     {id: 7, nombre: "SOFA CAMARO", cantidad: 1, precio: 36000, img: './img/BoC-HLamp2.jpg'},
